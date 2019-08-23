038289edad5740e627323307c001e606d3b6e173
virajransing <virajransing@gmail.com>
2019-08-23 23:09:47 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -47,12 +47,11 @@ end
 def checkout(cart, coupons)
     cd_cart = consolidate_cart(cart)
   cart_with_coupons_applied = apply_coupons(cd_cart, coupons)
-  cart_with_discounts_applied = apply_clearance
-    (cart_with_coupons_applied)
-    
-  total = 0.0 
+  cart_with_discounts_applied = apply_clearance(cart_with_coupons_applied)
+
+  total = 0.0
   cart_with_discounts_applied.keys.each do |item|
     total += cart_with_discounts_applied[item][:price]*cart_with_discounts_applied[item][:count]
-    end 
-    total > 100.00 ? (total * 0.90).round : total
+  end
+  total > 100.00 ? (total * 0.90).round : total
 end
